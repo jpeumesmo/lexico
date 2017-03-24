8a0d6dbe7499e55c85d351621930db91399a369a
jpeumesmo <joaopmoferreira@gmail.com>
2017-03-23 21:56:15 -0300

    commit com mudanças sobre abertura do arquivo e leitura de caracters

--- lexico.py
+++ lexico.py
@@ -3,8 +3,21 @@
 * "THE BEER-WARE LICENSE" (Revision 42):
 * <joaopmoferreira@gmail.com> wrote this file. As long as you retain this notice
 * you can do whatever you want with this stuff. If we meet some day, and you
-* think this stuff is worth it, you can buy me a beer in return, João Pedro
+* think this stuff is worth it, you can buy me a beer in return, Joao Pedro
 * Moreira Ferreira
 * ----------------------------------------------------------------------------
 '''
-import re
+import re,sys
+
+
+
+codigoFonte = open(sys.argv[1], 'r')
+
+texto = codigoFonte.read(1)
+print (texto)
+#texto = codigoFonte.readlines()
+#for linha in texto:
+#    print (linha)
+
+
+codigoFonte.close
