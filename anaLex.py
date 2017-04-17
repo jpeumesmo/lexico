12d57c57dd06e0a998857fe0f84460246b03250a
jpeumesmo <joaopmoferreira@gmail.com>
2017-04-17 01:19:30 -0300

    atualizacao com geracao de uma saida teste, e codigo reconhecendo todos os tokens

--- anaLex.py
+++ anaLex.py
@@ -17,10 +17,11 @@ numerais = []
 literais = []
 separadores = []
 reservadas = []
+operadores = []
 
 ################################ABERTURA DO ARQ##############################
 codigoFonte = open(sys.argv[1], 'r')
-
+tabelas = open("/home/jpeumesmo/UFSJ/Compiladores/lexico/saida.txt", 'w')
 ########################VARIAVEIS CONTADORAS E AUXILIARES#####################
 contLinha = 1
 contColuna = 0
@@ -50,56 +51,223 @@ else:
                 contColuna = contColuna + 1
                 if re.match("\d",caracter) is not None:
                     token = token + caracter
-                else if re.match("\.",caracter) is not None and flag == 0:
+                elif re.match("\.",caracter) is not None and flag == 0:
                     flag = 1
                     token = token + caracter
+                elif re.match("\n",caracter):
+                    contLinha = contLinha + 1
                 else:
                     numerais.append(token)
+                    caracter = codigoFonte.read(1)
+                    contColuna = contColuna + 1
                     break;
-        else if re.match("\"",caracter) is not None:
+
+    #    elif re.match("\#",caracter) is not None:
+            ############include encontrado#############
+        elif re.match("\"",caracter) is not None:
             ###########LITERAL ENCONTRADO##################
             token = token + caracter
             while True:
                 caracter = codigoFonte.read(1)
+                contColuna = contColuna + 1
                 if re.match("\"",caracter) is not None:
                     token = token + caracter
                     literais.append(token)
+                    caracter = codigoFonte.read(1)
+                    contColuna = contColuna + 1
                     break
+                elif re.match("\n",caracter):
+                    contLinha = contLinha + 1
                 else:
-                    token = toke + caracter
-        else if re.match("\<|\>|\&|\!|\|",caracter) is not None:
+                    token = token + caracter
+        elif re.match("\<|\>",caracter) is not None:
             ############OPERACAO LOGICA ENCOTRADA############
-
-        else if re.match("\=",caracter) is not None:
-            ############ATRIBUICAO OU EQUIVALENCIA ENCONTRADA#########
-
-        else if re.match("\+|\-|\*|\^|\\",caracter) is not None:
-            ###########OPERACAO ARITIMETICA ENCONTRADA##############
-
-        else if re.match("\/",caracter) is not None:
-            ################COMENTARIO ENCONTRADO###################
+            token = token + caracter
             caracter = codigoFonte.read(1)
+            contColuna = contColuna + 1
+            if re.match("\=",caracter) is not None:
+                token = token + caracter
+                operadores.append(token)
+                caracter = codigoFonte.read(1)
+                contColuna = contColuna + 1
+            elif re.match("\n",caracter):
+                contLinha = contLinha + 1
+                caracter = codigoFonte.read(1)
+                contColuna = contColuna + 1
+
+        elif re.match("\!",caracter) is not None:
+            ###########OPERADOR DE DIFERENTE LOGICO ENCONTRADO######
+            token = token + caracter
+            caracter = codigoFonte.read(1)
+            contColuna = contColuna + 1
+            if re.match("\=",caracter) is not None:
+                token = token + caracter
+                operadores.append(token)
+                caracter = codigoFonte.read(1)
+                contColuna = contColuna + 1
+            elif re.match("\n",caracter):
+                contLinha = contLinha + 1
+                caracter = codigoFonte.read(1)
+                contColuna = contColuna + 1
+            else:
+                sys.exit("Erro! 2" )
+
+        elif re.match("\&",caracter) is not None:
+            ##########ADN LOGICO ENCONTRADO#############
+            token = token + caracter
+            caracter = codigoFonte.read(1)
+            contColuna = contColuna + 1
+            if re.match("\&",caracter) is not None:
+                token = token + caracter
+                operadores.append(token)
+                caracter = codigoFonte.read(1)
+                contColuna = contColuna + 1
+            elif re.match("\n",caracter):
+                contLinha = contLinha + 1
+                caracter = codigoFonte.read(1)
+                contColuna = contColuna + 1
+            else:
+                sys.exit("Erro! 3" )
+
+        elif re.match("\|",caracter) is not None:
+            ############# OU LOGICO ENCONTRADO###########
+            token = token + caracter
+            caracter = codigoFonte.read(1)
+            contColuna = contColuna + 1
+            if re.match("\|",caracter) is not None:
+                token = token + caracter
+                operadores.append(token)
+                caracter = codigoFonte.read(1)
+                contColuna = contColuna + 1
+            elif re.match("\n",caracter):
+                contLinha = contLinha + 1
+                caracter = codigoFonte.read(1)
+                contColuna = contColuna + 1
+            else:
+                sys.exit("Erro! 4" )
+
+        elif re.match("\=",caracter) is not None:
+            ############ATRIBUICAO OU EQUIVALENCIA ENCONTRADA#########
+            token = token + caracter
+            caracter = codigoFonte.read(1)
+            contColuna = contColuna + 1
+            if re.match("\=",caracter) is not None:
+                token = token + caracter
+                operadores.append(token)
+                caracter = codigoFonte.read(1)
+                contColuna = contColuna + 1
+            elif re.match("\n",caracter):
+                contLinha = contLinha + 1
+                caracter = codigoFonte.read(1)
+                contColuna = contColuna + 1
+            else:
+                operadores.append(token)
+        elif re.match("\+",caracter):
+            #############SOMA OU INCREMENTO ENCONTRADO###############
+            token = token + caracter
+            caracter = codigoFonte.read(1)
+            contColuna = contColuna + 1
+            if re.match("\+",caracter) is not None:
+                token = token + caracter
+                operadores.append(token)
+                caracter = codigoFonte.read(1)
+                contColuna = contColuna + 1
+            elif re.match("\n",caracter):
+                contLinha = contLinha + 1
+                caracter = codigoFonte.read(1)
+                contColuna = contColuna + 1
+            else:
+                operadores.append(token)
+
+        elif re.match("\-",caracter):
+            ###########SUBTRACAO OU DECREMENTO ENCONTRADO############
+            token = token + caracter
+            caracter = codigoFonte.read(1)
+            contColuna = contColuna + 1
+            if re.match("\-",caracter) is not None:
+                token = token + caracter
+                operadores.append(token)
+                caracter = codigoFonte.read(1)
+                contColuna = contColuna + 1
+                break
+            elif re.match("\n",caracter):
+                contLinha = contLinha + 1
+                caracter = codigoFonte.read(1)
+                contColuna = contColuna + 1
+            else:
+                operadores.append(token)
+
+        elif re.match("\*|\^",caracter) is not None:
+            ###########OPERACAO ARITIMETICA ENCONTRADA##############
+            token = token + caracter
+            caracter = codigoFonte.read(1)
+            contColuna = contColuna + 1
+            if re.match("[a-z]|[A-Z]|\_|\s",caracter) is not None: #DIVISAO
+                operadores.append(token)
+                caracter = codigoFonte.read(1)
+                contColuna = contColuna + 1
+            elif re.match("\n",caracter):
+                contLinha = contLinha + 1
+            else:
+                sys.exit("Erro! 5" )
+        elif re.match("\/",caracter) is not None:
+            ################COMENTARIO OU DIVISAO ENCONTRADO###################
+            token = token + caracter
+            caracter = codigoFonte.read(1)
+            contColuna = contColuna + 1
             if re.match("\/",caracter) is not None: #COMENTARIO DE LINHA
                 while True:
                     caracter = codigoFonte.read(1)
-                    if (caracter == "\n")
+                    if re.match("\n",caracter):
                         contLinha = contLinha + 1
+                        caracter = codigoFonte.read(1)
                         break
-            if re.match("\*",caracter) is not None: #COMENTARIO DE BLOCO
+            elif re.match("\*",caracter) is not None: #COMENTARIO DE BLOCO
                 while True:
                     caracter = codigoFonte.read(1)
+                    contColuna = contColuna + 1
+                    if re.match("\n",caracter):
+                        contLinha = contLinha + 1
                     if re.match("\*",caracter) is not None:
                         caracter = codigoFonte.read(1)
                         if re.match("\/",caracter) is not None:
+                            caracter = codigoFonte.read(1)
                             break
+            elif re.match("[a-z]|[A-Z]|\_|\s",caracter) is not None: #DIVISAO
+                operadores.append(token)
+                caracter = codigoFonte.read(1)
+                contColuna = contColuna + 1
+            elif re.match("\n",caracter):
+                contLinha = contLinha + 1
+                caracter = codigoFonte.read(1)
+                contColuna = contColuna + 1
+            else:
+                sys.exit("Erro! 6" )
 
-        else if re.match("\;|\[|\(|\{|\,|.")
+        elif re.match("\;|\[|\(|\{|\,|.|\n|\t|\s",caracter) is not None:
         ##################SEPARADOR ENCONTRADO####################
             token = token + caracter
-            separadores.appen(token)
+            separadores.append(token)
+            caracter = codigoFonte.read(1)
+            contColuna = contColuna + 1
 
-        else if: re.match("[a-z]|[A-Z]|\_",caracter)
-        ###############IDENTIFICADOR OU PALAVRA RESERVADA ENCONTRADA
+        elif re.match("[a-z]|[A-Z]|\_",caracter) is not None:
+        ###############IDENTIFICADOR OU PALAVRA RESERVADA ENCONTRADA######
+            token = token + caracter
+            while True:
+                caracter = codigoFonte.read(1)
+                contColuna = contColuna + 1
+                if re.match("[a-z]|[A-Z]|\_",caracter) is not None:
+                    token = token + caracter
+                else:
+                    break
+            if re.match("if|else|while|return|continue|break|int|char|float|for",token) is not None:
+                reservadas.append(token)
+            else:
+                identificadores.append(token)
+
         else:
-            print("Erro!")
-print (numerais)
+            sys.exit("Erro! 7" )
+
+for i in reservadas:
+    tabelas.write(i)
